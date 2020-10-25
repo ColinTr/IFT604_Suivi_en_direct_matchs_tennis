ab8fec2b64f94f8f32eeef6484dd29f8a1004701
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-10-25 15:48:56 +0100

    Matricules et noms ajoutés dans toutes les classes

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+ * Troisemaine Colin (matricule 20 088 209)
+ * Levieux Quentin (matricule 20 102 087)
+ * Verdier Adrien (matricule 20 088 959)
+ * Alexandre Turpin (matricule 20 088 156)
+ */
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
--- src/generateur.js
+++ src/generateur.js
@@ -1,3 +1,10 @@
+/**
+ * Troisemaine Colin (matricule 20 088 209)
+ * Levieux Quentin (matricule 20 102 087)
+ * Verdier Adrien (matricule 20 088 959)
+ * Alexandre Turpin (matricule 20 088 156)
+ */
+
 const Partie = require('./modeles/partie');
 const Joueur = require('./modeles/joueur');
 
