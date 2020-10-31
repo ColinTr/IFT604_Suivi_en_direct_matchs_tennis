30dadc3de2c3b6f2af291c99c2e1156ad2beb601
Alexandre Turpin <alexandre.turpin@outlook.com>
2020-10-31 13:54:45 +0100

    Finition interface resume Match en cours + factorisation du code pour le rafraichissement de la page + ajout du bouton refresh, il faut faire son code

--- application_android/app/src/main/java/com/example/tennisbet/ResumePartie.java
+++ application_android/app/src/main/java/com/example/tennisbet/ResumePartie.java
@@ -16,10 +16,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.tennisbet.modele.Echange;
+import com.example.tennisbet.modele.Jeu;
+import com.example.tennisbet.modele.Manche;
 import com.example.tennisbet.modele.Partie;
 
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+
+import static java.lang.Math.abs;
 
 public class ResumePartie extends AppCompatActivity {
 
@@ -37,8 +42,33 @@ public class ResumePartie extends AppCompatActivity {
 
         TextView tv_joueur1 = (TextView) findViewById(R.id.tv_joueur1);
         TextView tv_joueur2 = (TextView) findViewById(R.id.tv_joueur2);
+        TextView tv_joueur1_2 = (TextView) findViewById(R.id.tv_joueur1_2);
+        TextView tv_joueur2_2 = (TextView) findViewById(R.id.tv_joueur2_2);
+        tv_joueur1_2.setText(partie.getJoueur_1().getPrenom().charAt(0) + "." + partie.getJoueur_1().getNom());
+        tv_joueur2_2.setText(partie.getJoueur_2().getPrenom().charAt(0) + "." + partie.getJoueur_2().getNom());
+
         tv_joueur1.setText(partie.getJoueur_1().getPrenom().charAt(0) + "." + partie.getJoueur_1().getNom() + " (" + partie.getJoueur_1().getRang() + ")");
         tv_joueur2.setText(partie.getJoueur_2().getPrenom().charAt(0) + "." + partie.getJoueur_2().getNom() + " (" + partie.getJoueur_2().getRang() + ")");
+
+        miseAJourAfficahge();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void miseAJourAfficahge() {
+
+        miseAJourTableauDesScores();
+
+        //Affichage si la partie est terminee
+        if(partie.getEtat_partie() == 2)
+        {
+            miseAJourInformationsPartieTermine();
+        }
+        else {
+            miseAJourInformationPartieEnCours();
+        }
+    }
+
+    public void miseAJourTableauDesScores() {
 
         TextView tv_score_set_r1_c1 = (TextView) findViewById(R.id.tv_score_set_r1_c1);
         TextView tv_score_set_r1_c2 = (TextView) findViewById(R.id.tv_score_set_r1_c2);
@@ -46,48 +76,6 @@ public class ResumePartie extends AppCompatActivity {
         TextView tv_score_set_r2_c1 = (TextView) findViewById(R.id.tv_score_set_r2_c1);
         TextView tv_score_set_r2_c2 = (TextView) findViewById(R.id.tv_score_set_r2_c2);
         TextView tv_score_set_r2_c3 = (TextView) findViewById(R.id.tv_score_set_r2_c3);
-
-        TextView tv_text_etat_match = (TextView) findViewById(R.id.tv_text_etat_match);
-        TextView tv_temps_partie = (TextView) findViewById(R.id.tv_temps_partie);
-
-        TextView tv_service_joueur_1 = (TextView) findViewById(R.id.tv_service_joueur_1);
-        TextView tv_service_joueur_2 = (TextView) findViewById(R.id.tv_service_joueur_2);
-
-        TextView tv_points_joueur_1 = (TextView) findViewById(R.id.tv_points_joueur_1);
-        TextView tv_points_joueur_2 = (TextView) findViewById(R.id.tv_points_joueur_2);
-
-        TextView tv_contestation_joueur_1 = (TextView) findViewById(R.id.tv_contestation_joueur_1);
-        TextView tv_contestation_joueur_2 = (TextView) findViewById(R.id.tv_contestation_joueur_2);
-
-        Button btn_parier_joueur_1 = (Button) findViewById(R.id.btn_parier_joueur_1);
-        Button btn_parier_joueur_2 = (Button) findViewById(R.id.btn_parier_joueur_2);
-
-        /*if(partie.getEtat_partie() == 2)
-        {
-            tv_service_joueur_1.setVisibility(TextView.INVISIBLE);
-            tv_service_joueur_2.setVisibility(TextView.INVISIBLE);
-
-            tv_contestation_joueur_1.setVisibility(TextView.INVISIBLE);
-            tv_contestation_joueur_2.setVisibility(TextView.INVISIBLE);
-
-            btn_parier_joueur_1.setVisibility(TextView.INVISIBLE);
-            btn_parier_joueur_2.setVisibility(TextView.INVISIBLE);
-
-            switch (partie.getJoueur_gagnant()) {
-                case 1:
-                    tv_joueur1.setTypeface(null, Typeface.BOLD);
-                    break;
-                case 2:
-                    tv_joueur2.setTypeface(null, Typeface.BOLD);
-                    break;
-            }
-            long seconds = ChronoUnit.SECONDS.between(partie.getDatetime_debut_partie(), partie.getDateTime_fin_partie());
-            LocalTime timeOfDay = LocalTime.ofSecondOfDay(seconds);
-            String time = timeOfDay.toString();
-            tv_temps_partie.setText(time);
-
-            tv_text_etat_match.setText("Match Terminé");
-        }*/
 
         int nb_manches_joue = partie.getScore_manche().size();
 
@@ -173,6 +161,118 @@ public class ResumePartie extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void miseAJourInformationsPartieTermine() {
+        TextView tv_text_etat_match = (TextView) findViewById(R.id.tv_text_etat_match);
+        TextView tv_temps_partie = (TextView) findViewById(R.id.tv_temps_partie);
+
+        TextView tv_joueur1 = (TextView) findViewById(R.id.tv_joueur1);
+        TextView tv_joueur2 = (TextView) findViewById(R.id.tv_joueur2);
+
+        ((TextView) findViewById(R.id.tv_service_joueur_1)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.tv_service_joueur_2)).setVisibility(TextView.INVISIBLE);
+
+        ((TextView) findViewById(R.id.tv_contestation_joueur_1)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.tv_contestation_joueur_2)).setVisibility(TextView.INVISIBLE);
+
+        ((TextView) findViewById(R.id.btn_parier_joueur_1)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.btn_parier_joueur_2)).setVisibility(TextView.INVISIBLE);
+
+        ((TextView) findViewById(R.id.tv_joueur1_2)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.tv_joueur2_2)).setVisibility(TextView.INVISIBLE);
+
+        ((TextView) findViewById(R.id.tv_points_joueur_1)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.tv_points_joueur_2)).setVisibility(TextView.INVISIBLE);
+
+        ((TextView) findViewById(R.id.tv_text_coups_echange)).setVisibility(TextView.INVISIBLE);
+        ((TextView) findViewById(R.id.tv_coups_echange)).setVisibility(TextView.INVISIBLE);
+
+        switch (partie.getJoueur_gagnant()) {
+            case 1:
+                tv_joueur1.setTypeface(null, Typeface.BOLD);
+                break;
+            case 2:
+                tv_joueur2.setTypeface(null, Typeface.BOLD);
+                break;
+        }
+        long seconds = ChronoUnit.SECONDS.between(partie.getDatetime_debut_partie(), partie.getDateTime_fin_partie());
+        LocalTime timeOfDay = LocalTime.ofSecondOfDay(seconds);
+        String time = timeOfDay.toString();
+        tv_temps_partie.setText(time);
+
+        tv_text_etat_match.setText("Match Terminé");
+    }
+
+    public void miseAJourInformationPartieEnCours() {
+        Manche mancheEnCours = null;
+        Jeu jeuEnCours = null;
+        Echange echangeEnCours = null;
+
+        for(Manche manche : partie.getScore_manche()){
+            if(!manche.isEtat_manche()){
+                mancheEnCours = manche;
+            }
+        }
+
+        for(Jeu jeu : mancheEnCours.getScore_jeu()){
+            if(!jeu.isEtat_jeu()){
+                jeuEnCours = jeu;
+            }
+        }
+
+        for(Echange echange : jeuEnCours.getEchangeList()){
+            if(!echange.isEtat_echange()){
+                echangeEnCours = echange;
+            }
+        }
+
+        TextView tv_service_joueur_1 = (TextView) findViewById(R.id.tv_service_joueur_1);
+        TextView tv_service_joueur_2 = (TextView) findViewById(R.id.tv_service_joueur_2);
+
+        TextView tv_contestation_joueur_1 = (TextView) findViewById(R.id.tv_contestation_joueur_1);
+        TextView tv_contestation_joueur_2 = (TextView) findViewById(R.id.tv_contestation_joueur_2);
+
+        ((TextView) findViewById(R.id.tv_points_joueur_1)).setText(String.valueOf(jeuEnCours.getScore_echange_joueur_1()));
+        ((TextView) findViewById(R.id.tv_points_joueur_2)).setText(String.valueOf(jeuEnCours.getScore_echange_joueur_2()));
+
+        if(jeuEnCours.getJoueur_au_service().equals(partie.getJoueur_1())){
+            tv_service_joueur_1.setText("Service (" + String.valueOf(echangeEnCours.getVitesse_service()) + " km/h)");
+            tv_service_joueur_2.setVisibility(View.INVISIBLE);
+        }
+        else {
+            tv_service_joueur_2.setText("Service (" + String.valueOf(echangeEnCours.getVitesse_service()) + " km/h)");
+            tv_service_joueur_1.setVisibility(View.INVISIBLE);
+        }
+
+        tv_contestation_joueur_1.setVisibility(View.INVISIBLE);
+        tv_contestation_joueur_2.setVisibility(View.INVISIBLE);
+
+        if(echangeEnCours.getConteste_par_joueur().equals(partie.getJoueur_1())){
+            if(echangeEnCours.isContestation_acceptee()){
+                tv_contestation_joueur_1.setText("Contestation acceptée");
+                tv_contestation_joueur_1.setTextColor(Color.parseColor("#28A745"));
+            } else {
+                tv_contestation_joueur_1.setText("Contestation refusée");
+                tv_contestation_joueur_1.setTextColor(Color.parseColor("#EE0000"));
+            }
+
+            tv_contestation_joueur_1.setVisibility(View.VISIBLE);
+        }
+        else if(echangeEnCours.getConteste_par_joueur().equals(partie.getJoueur_2())){
+            if(echangeEnCours.isContestation_acceptee()){
+                tv_contestation_joueur_2.setText("Contestation acceptée");
+                tv_contestation_joueur_2.setTextColor(Color.parseColor("#28A745"));
+            } else {
+                tv_contestation_joueur_2.setText("Contestation refusée");
+                tv_contestation_joueur_2.setTextColor(Color.parseColor("#EE0000"));
+            }
+            tv_contestation_joueur_2.setVisibility(View.VISIBLE);
+        }
+
+        ((TextView) findViewById(R.id.tv_coups_echange)).setText(String.valueOf(echangeEnCours.getNombre_coup_echangee()));
+
+    }
+
     public void parierSurJoueur1(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
@@ -225,5 +325,8 @@ public class ResumePartie extends AppCompatActivity {
                 });
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    public void rafraichirMatch(View view) {
     }
 }
