9420ac4ed4e9d1bdbf5af2908617439d9547dcf8
Alexandre Turpin <alexandre.turpin@outlook.com>
2020-11-10 16:15:44 +0100

    Changement titre joueur

--- serveur_react/src/components/Joueurs.js
+++ serveur_react/src/components/Joueurs.js
@@ -5,7 +5,9 @@ class Joueurs extends Component {
     render()
     {
         return(
-            <div>La liste des joueurs</div>
+            <div className="pageJoueurs">
+                <h1 className="text-center titreJoueurs">La liste des joueurs</h1>
+            </div>
         )
     }
 }
