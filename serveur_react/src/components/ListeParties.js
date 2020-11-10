22b7e986066883547246233919ed43daef9f6c63
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-11-10 12:38:43 +0100

    Amélioration de la liste des parties du jour

--- serveur_react/src/components/ListeParties.js
+++ serveur_react/src/components/ListeParties.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PartieDansListe from './PartieDansListe'
 import axios from 'axios'
+import {MDBContainer, MDBTable, MDBTableBody} from "mdbreact";
 
 class ListeParties extends Component {
     // Adds a class constructor that assigns the initial state values:
     constructor(props) {
         super(props);
         this.state = {
-            listePartiesData : []
+            listePartiesData: []
         };
     }
 
     // This is called when an instance of a component is being created and inserted into the DOM.
     componentDidMount() {
         if (!navigator.onLine) {
-            this.setState({ listePartiesData: localStorage.getItem('listePartiesData') });
+            this.setState({listePartiesData: localStorage.getItem('listePartiesData')});
         }
 
         this.updateListeParties();
@@ -24,10 +25,10 @@ class ListeParties extends Component {
         }, 10000);
     }
 
-    updateListeParties(){
+    updateListeParties() {
         axios.get('http://localhost:3000/parties')
             .then(response => {
-                this.setState({ listePartiesData: response.data });
+                this.setState({listePartiesData: response.data});
                 localStorage.setItem('listePartiesData', response.data);
             })
             // Catch any error here
@@ -39,12 +40,16 @@ class ListeParties extends Component {
     // The render method contains the JSX code which will be compiled to HTML.
     render() {
         return (
-            <div className="list-group">
-                {this.state.listePartiesData.map(function (partieData, index) {
-                        return <PartieDansListe key={partieData.id_partie} data={partieData}/>
-                    }
-                )}
-            </div>
+            <MDBContainer style={{width: '30%'}} >
+                <MDBTable className="sticky-top">
+                    <MDBTableBody>
+                        {this.state.listePartiesData.map(function (partieData, index) {
+                                return <PartieDansListe key={partieData.id_partie} data={partieData}/>
+                            }
+                        )}
+                    </MDBTableBody>
+                </MDBTable>
+            </MDBContainer>
         );
     }
 }
--- serveur_react/src/components/PartieDansListe.js
+++ serveur_react/src/components/PartieDansListe.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {MDBRow, MDBCol, MDBContainer} from "mdbreact";
 
 class PartieDansListe extends Component {
     constructor(props) {
@@ -13,36 +14,41 @@ class PartieDansListe extends Component {
 
     render() {
         return (
-            <a href="#!" className="list-group-item list-group-item-action flex-column align-items-start">
-                <table>
-                    <tbody>
-                    <tr>
-                        <td className="alnleft">
-                            <p className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>{this.state.nomJoueur1}</p>
-                        </td>
-                        <td className="alnright">
-                            {this.state.listeManches.map(manche => {
-                                return(
-                                    <button key={manche.id_manche} type="button" className={manche.score_jeux_joueur_1===6?"btn btn-success":"btn btn-dark"}>{manche.score_jeux_joueur_1}</button>
-                                )
-                            })}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="alnleft">
-                            <p className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>{this.state.nomJoueur2}</p>
-                        </td>
-                        <td className="alnright">
-                            {this.state.listeManches.map(manche => {
-                                return(
-                                    <button key={manche.id_manche} type="button" className={manche.score_jeux_joueur_2===6?"btn btn-success":"btn btn-dark"}>{manche.score_jeux_joueur_2}</button>
-                                )
-                            })}
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </a>
+            <tr>
+                <td>
+                    <MDBRow>
+                        <MDBContainer className='d-flex align-items-center'>
+                            <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
+                                {this.state.nomJoueur1}
+                            </MDBCol>
+                            <MDBCol>
+                                {this.state.listeManches.map(manche => {
+                                    return (
+                                        <button key={manche.id_manche} type="button"
+                                                className={manche.score_jeux_joueur_1 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_1}</button>
+                                    )
+                                })}
+                            </MDBCol>
+                        </MDBContainer>
+                    </MDBRow>
+                    <MDBRow>
+                        <MDBContainer className='d-flex align-items-center'>
+                            <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
+                                {this.state.nomJoueur2}
+                            </MDBCol>
+                            <MDBCol>
+                                {this.state.listeManches.map(manche => {
+                                    return (
+                                        <button key={manche.id_manche} type="button"
+                                                className={manche.score_jeux_joueur_2 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_2}</button>
+                                    )
+                                })}
+                            </MDBCol>
+                        </MDBContainer>
+                    </MDBRow>
+                </td>
+            </tr>
+
         );
     }
 }
