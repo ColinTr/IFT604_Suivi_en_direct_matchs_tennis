c7b6d9ae3e373f17059b71fafe8cf2354f00263e
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-11-10 12:48:46 +0100

    random fixes

--- serveur_react/src/components/Login.js
+++ serveur_react/src/components/Login.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import React, {Component} from 'react';
+import {MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn} from 'mdbreact';
 import logo from '../assets/images/tennis_raquette.png'
-import { askForPermissionToReceiveNotifications } from '../notification/push-notification';
+import {askForPermissionToReceiveNotifications} from '../notification/push-notification';
 import axios from 'axios'
 
 class Login extends Component {
@@ -16,26 +16,22 @@ class Login extends Component {
     }
 
     handleChangeNom = event => {
-        const {name, value } = event.target
+        const {name, value} = event.target;
         this.setState({
             [name]: value
         })
-    }
+    };
 
     Connexion() {
         let that = this;
         console.log(this.state.nom_utilisateur);
-        if (this.state.nom_utilisateur === ""){
+        if (this.state.nom_utilisateur === "") {
             document.getElementById("NomVide").style.display = 'block';
-        }
-        else if (this.state.nom_utilisateur !== "") {
+        } else if (this.state.nom_utilisateur !== "") {
             askForPermissionToReceiveNotifications().then(function (result) {
                 that.setState({token: result});
-                console.log(that.state.token);
-
-                console.log("right ?");
+                console.log("Token : " + that.state.token);
                 let url = 'http://localhost:3000/utilisateurs/' + that.state.nom_utilisateur + '/' + that.state.token;
-                console.log(url);
                 axios.get(url)
                     .then(response => {
                         console.log(response.data);
@@ -44,31 +40,30 @@ class Login extends Component {
                     .catch(error => {
                         console.log(error)
                     });
-
-                console.log("why !");
             })
         }
     }
 
-    render()
-    {
+    render() {
         return (
             <MDBContainer>
                 <MDBRow className="d-flex justify-content-center">
                     <MDBCol md="6">
                         <form>
                             <div className="text-center">
-                                <img src={logo} alt="Logo" width="125px" height="100px"></img>
+                                <img src={logo} alt="Logo" height="100px"/>
                             </div>
-                            <p className="h5 text-center mb-4">Nom d'Utilisateur</p>
+                            <p className="h5 text-center mb-4">Connexion</p>
                             <div className="grey-text">
-                                <MDBInput label="Nom" validate error="wrong" success="right" name="nom_utilisateur" value={this.state.nom_utilisateur} onChange={this.handleChangeNom}/>
+                                <MDBInput label="Nom d'utilisateur" validate error="wrong" success="right" name="nom_utilisateur"
+                                          value={this.state.nom_utilisateur} onChange={this.handleChangeNom}/>
                             </div>
                             <div className="text-center">
                                 <MDBBtn onClick={this.Connexion}>Entrer</MDBBtn>
                             </div>
                             <div className="red-text">
-                                <p className="text-center" style={{display : 'none'}} id="NomVide">Merci de rentrer un nom d'utilisateur</p>
+                                <p className="text-center" style={{display: 'none'}} id="NomVide">Merci de rentrer un
+                                    nom d'utilisateur</p>
                             </div>
                         </form>
                     </MDBCol>
