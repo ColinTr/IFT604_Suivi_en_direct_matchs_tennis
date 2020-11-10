0165701391ed5f895e1e312007a12fea5f1272b5
Alexandre Turpin <alexandre.turpin@outlook.com>
2020-11-10 16:28:22 +0100

    Optimisation du return

--- serveur_react/src/components/PartieDansListe.js
+++ serveur_react/src/components/PartieDansListe.js
@@ -16,70 +16,68 @@ class PartieDansListe extends Component {
     }
 
     render() {
-        return(
-            (this.state.etat_partie === 1 || this.state.datetime_fin_partie !== null) ? (
-                <tr>
-                    <td style={{padding: 0}} className="linkPartieDansListe">
-                        <a href="/" className="lienVersPartie">
-                            <MDBRow>
-                                <MDBContainer className='listPartieRow'>
-                                    <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
-                                        {this.state.nomJoueur1}
-                                    </MDBCol>
-                                    <MDBCol className="listPartieRowItem">
-                                        {this.state.listeManches.map(manche => {
-                                            return (
-                                                <button key={manche.id_manche} type="button"
-                                                        className={manche.score_jeux_joueur_1 === 6 ? "btn btn-success unclickable" : "btn btn-dark unclickable"}>{manche.score_jeux_joueur_1}</button>
-                                            )
-                                        })}
-                                    </MDBCol>
-                                </MDBContainer>
-                            </MDBRow>
-                            <MDBRow>
-                                <MDBContainer className='listPartieRow'>
-                                    <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
-                                        {this.state.nomJoueur2}
-                                    </MDBCol>
-                                    <MDBCol className="listPartieRowItem">
-                                        {this.state.listeManches.map(manche => {
-                                            return (
-                                                <button key={manche.id_manche} type="button"
-                                                        className={manche.score_jeux_joueur_2 === 6 ? "btn btn-success unclickable" : "btn btn-dark unclickable"}>{manche.score_jeux_joueur_2}</button>
-                                            )
-                                        })}
-                                    </MDBCol>
-                                </MDBContainer>
-                            </MDBRow>
-                        </a>
-                    </td>
-                </tr>
-            )
-            : (
-                <tr>
-                    <td style={{padding: 0}} className="linkPartieDansListe">
-                        <a href="/" className="lienVersPartie">
-                            <MDBRow>
-                                <MDBContainer className='d-flex align-items-center'>
-                                    <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
-                                        {this.state.nomJoueur1}
-                                    </MDBCol>
-                                    <MDBCol className='font-weight-bold'>
-                                        {this.state.datetime_debut_partie.split(" ")[1]}
-                                    </MDBCol>
-                                </MDBContainer>
-                            </MDBRow>
-                            <MDBRow>
-                                <MDBContainer className='d-flex align-items-center'>
-                                    <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
-                                        {this.state.nomJoueur2}
-                                    </MDBCol>
-                                </MDBContainer>
-                            </MDBRow>
-                        </a>
-                    </td>
-                </tr>
-            )
+        return (this.state.etat_partie === 1 || this.state.datetime_fin_partie !== null) ? (
+            <tr>
+                <td style={{padding: 0}} className="linkPartieDansListe">
+                    <a href="/" className="lienVersPartie">
+                        <MDBRow>
+                            <MDBContainer className='listPartieRow'>
+                                <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur1}
+                                </MDBCol>
+                                <MDBCol className="listPartieRowItem">
+                                    {this.state.listeManches.map(manche => {
+                                        return (
+                                            <button key={manche.id_manche} type="button"
+                                                    className={manche.score_jeux_joueur_1 === 6 ? "btn btn-success unclickable" : "btn btn-dark unclickable"}>{manche.score_jeux_joueur_1}</button>
+                                        )
+                                    })}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBContainer className='listPartieRow'>
+                                <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur2}
+                                </MDBCol>
+                                <MDBCol className="listPartieRowItem">
+                                    {this.state.listeManches.map(manche => {
+                                        return (
+                                            <button key={manche.id_manche} type="button"
+                                                    className={manche.score_jeux_joueur_2 === 6 ? "btn btn-success unclickable" : "btn btn-dark unclickable"}>{manche.score_jeux_joueur_2}</button>
+                                        )
+                                    })}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                    </a>
+                </td>
+            </tr>
+        )
+        : (
+            <tr>
+                <td style={{padding: 0}} className="linkPartieDansListe">
+                    <a href="/" className="lienVersPartie">
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur1}
+                                </MDBCol>
+                                <MDBCol className='font-weight-bold'>
+                                    {this.state.datetime_debut_partie.split(" ")[1]}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur2}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                    </a>
+                </td>
+            </tr>
         )
     }
 }
