8f96f35313fe31fe1ff8964e83d3966cb123e3cd
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-11-10 16:27:06 +0100

    return statement missing

--- serveur_react/src/components/PartieDansListe.js
+++ serveur_react/src/components/PartieDansListe.js
@@ -16,8 +16,8 @@ class PartieDansListe extends Component {
     }
 
     render() {
-        (this.state.etat_partie === 1 || this.state.datetime_fin_partie !== null) ? 
-        ( 
+        return(
+            (this.state.etat_partie === 1 || this.state.datetime_fin_partie !== null) ? (
                 <tr>
                     <td style={{padding: 0}} className="linkPartieDansListe">
                         <a href="/" className="lienVersPartie">
@@ -54,8 +54,8 @@ class PartieDansListe extends Component {
                         </a>
                     </td>
                 </tr>
-        )
-        : (
+            )
+            : (
                 <tr>
                     <td>
                         <MDBRow>
@@ -78,6 +78,7 @@ class PartieDansListe extends Component {
                     </td>
                 </tr>
             )
+        )
     }
 }
 
