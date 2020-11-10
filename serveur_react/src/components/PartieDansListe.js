47b88d8ee9d9e890d2f586e4b950fe5a4bce2399
AdrienVerdier <adrien.verdier@gmail.com>
2020-11-10 16:07:18 +0100

    Affichage partie non commencé

--- serveur_react/src/components/PartieDansListe.js
+++ serveur_react/src/components/PartieDansListe.js
@@ -8,48 +8,77 @@ class PartieDansListe extends Component {
             nomJoueur1: props.data.joueur1.prenom + " " + props.data.joueur1.nom,
             nomJoueur2: props.data.joueur2.prenom + " " + props.data.joueur2.nom,
             joueurGagnant: props.data.joueur_gagnant,
-            listeManches: props.data.liste_manches
+            listeManches: props.data.liste_manches,
+            etat_partie: props.data.etat_partie,
+            datetime_debut_partie: props.data.datetime_debut_partie,
+            datetime_fin_partie: props.data.datetime_fin_partie
         };
     }
 
     render() {
-        return (
-            <tr>
-                <td>
-                    <MDBRow>
-                        <MDBContainer className='d-flex align-items-center'>
-                            <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
-                                {this.state.nomJoueur1}
-                            </MDBCol>
-                            <MDBCol>
-                                {this.state.listeManches.map(manche => {
-                                    return (
-                                        <button key={manche.id_manche} type="button"
-                                                className={manche.score_jeux_joueur_1 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_1}</button>
-                                    )
-                                })}
-                            </MDBCol>
-                        </MDBContainer>
-                    </MDBRow>
-                    <MDBRow>
-                        <MDBContainer className='d-flex align-items-center'>
-                            <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
-                                {this.state.nomJoueur2}
-                            </MDBCol>
-                            <MDBCol>
-                                {this.state.listeManches.map(manche => {
-                                    return (
-                                        <button key={manche.id_manche} type="button"
-                                                className={manche.score_jeux_joueur_2 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_2}</button>
-                                    )
-                                })}
-                            </MDBCol>
-                        </MDBContainer>
-                    </MDBRow>
-                </td>
-            </tr>
-
-        );
+        if (this.state.etat_partie === 1 || this.state.datetime_fin_partie !== null){
+            return (
+                <tr>
+                    <td>
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur1}
+                                </MDBCol>
+                                <MDBCol>
+                                    {this.state.listeManches.map(manche => {
+                                        return (
+                                            <button key={manche.id_manche} type="button"
+                                                    className={manche.score_jeux_joueur_1 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_1}</button>
+                                        )
+                                    })}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur2}
+                                </MDBCol>
+                                <MDBCol>
+                                    {this.state.listeManches.map(manche => {
+                                        return (
+                                            <button key={manche.id_manche} type="button"
+                                            className={manche.score_jeux_joueur_2 === 6 ? "btn btn-success" : "btn btn-dark"}>{manche.score_jeux_joueur_2}</button>
+                                        )
+                                    })}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                    </td>
+                </tr>
+            )
+        }
+        else {
+            return (
+                <tr>
+                    <td>
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 1 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur1}
+                                </MDBCol>
+                                <MDBCol className='font-weight-bold'>
+                                    {this.state.datetime_debut_partie.split(" ")[1]}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBContainer className='d-flex align-items-center'>
+                                <MDBCol className={this.state.joueurGagnant === 2 ? "font-weight-bold" : ""}>
+                                    {this.state.nomJoueur2}
+                                </MDBCol>
+                            </MDBContainer>
+                        </MDBRow>
+                    </td>
+                </tr>
+            )
+        }
     }
 }
 
