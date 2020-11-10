c5af332d9d5060158ff2b64c5388b434753d86b9
AdrienVerdier <adrien.verdier@gmail.com>
2020-11-10 18:54:37 +0100

    Ajout Bouton Parier

--- serveur_react/src/components/PartieDetaillee.js
+++ serveur_react/src/components/PartieDetaillee.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBCard} from "mdbreact";
+import {MDBContainer, MDBRow, MDBCol, MDBCard, MDBBtn} from "mdbreact";
 import axios from "axios";
+import Swal from 'sweetalert2';
 
 class PartieDetaillee extends Component{
 
@@ -9,7 +10,9 @@ class PartieDetaillee extends Component{
         this.state = {
             idPartie : props.match.params.idPartie,
             nomJoueur1: "",
+            idJoueur1: 0,
             nomJoueur2: "",
+            idJoueur2: 0,
             rangJoueur1: "",
             rangJoueur2: "",
             dureePartieSecondes : "",
@@ -39,7 +42,9 @@ class PartieDetaillee extends Component{
                 console.log(response.data)
                 this.setState({
                     nomJoueur1 : data.joueur1.prenom + " " + data.joueur1.nom,
+                    idJoueur1 : data.joueur1.id_joueur,
                     nomJoueur2 : data.joueur2.prenom + " " + data.joueur2.nom,
+                    idJoueur2 : data.joueur2.id_joueur,
                     rangJoueur1 : data.joueur1.rang,
                     rangJoueur2 : data.joueur2.rang,
                     dureePartieSecondes : data.duree_partie,
@@ -61,6 +66,84 @@ class PartieDetaillee extends Component{
         hour /= 60;
 
         return hour.toFixed(0)+" : "+minutes.toFixed(0)+" : "+newSeconds
+    }
+
+    ParieJoueur1 = async () => {
+        const {value: montant_parie} = await Swal.fire({
+            title: 'Parier',
+            text: 'Combien voulez-vous parier sur le joueur 1',
+            icon: 'info',
+            input: 'text',
+            inputLabel: 'Montant du pari',
+            confirmButtonText: 'Parier',
+            cancelButtonText: 'Annuler',
+            showCancelButton: true,
+            inputValidator: (inputValue) => {
+                if (!inputValue) {
+                    return 'Veuillez rentrer une valeur'
+                }
+                if (isNaN(inputValue)) {
+                    return 'Veuillez rentrer un chiffre'
+                }
+            }
+        });
+
+        if (montant_parie) {
+            const paris = {
+                id_utilisateur: localStorage.getItem('idUtilisateur'),
+                id_partie: this.state.idPartie,
+                id_joueur: this.state.idJoueur1,
+                montant_pari: parseInt(montant_parie)
+            };
+
+            axios.post('http://localhost:3000/paris', paris)
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        }
+    }
+
+    ParieJoueur2 = async() => {
+        const {value: montant_parie } = await Swal.fire({
+            title: 'Parier',
+            text: 'Combien voulez-vous parier sur le joueur 2',
+            icon: 'info',
+            input: 'text',
+            inputLabel: 'Montant du pari',
+            confirmButtonText: 'Parier',
+            cancelButtonText: 'Annuler',
+            showCancelButton: true,
+            inputValidator: (inputValue) => {
+                if(!inputValue){
+                    return 'Veuillez rentrer une valeur'
+                }
+                if(isNaN(inputValue)){
+                    return 'Veuillez rentrer un chiffre'
+                }
+            }
+        });
+
+        if (montant_parie) {
+            const paris = {
+                id_utilisateur: localStorage.getItem('idUtilisateur'),
+                id_partie: this.state.idPartie,
+                id_joueur: this.state.idJoueur2,
+                montant_pari: parseInt(montant_parie)
+            };
+
+            axios.post('http://localhost:3000/paris', paris)
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        }
     }
 
     render(){
@@ -115,6 +198,19 @@ class PartieDetaillee extends Component{
                             <MDBRow >
                                 <MDBCol className="d-flex justify-content-center" sm="6">Contestations</MDBCol>
                                 <MDBCol className="d-flex justify-content-center" sm="6">Contestations</MDBCol>
+                            </MDBRow>
+
+                            <MDBRow>
+                                <MDBCol className="d-flex justify-content-center" sm="6">
+                                    {(this.state.etatPartie === 0 || (this.state.etatPartie === 1 && this.state.listeManches.length <= 1)) ?
+                                        <MDBBtn onClick={this.ParieJoueur1}>Parier</MDBBtn>
+                                        : <div></div> }
+                                </MDBCol>
+                                <MDBCol className="d-flex justify-content-center" sm="6">
+                                    {(this.state.etatPartie === 0 || (this.state.etatPartie === 1 && this.state.listeManches.length <= 1)) ?
+                                        (<MDBBtn onClick={this.ParieJoueur2}>Parier</MDBBtn>)
+                                        : (<div></div>) }
+                                </MDBCol>
                             </MDBRow>
 
                         </MDBCard>
