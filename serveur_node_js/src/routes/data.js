669cb08b0fd1b57c1d2e33196cdee2610b83e7f8
Quentin-56 <kantin56@yahoo.fr>
2020-12-03 12:28:38 +0100

    Implementation route /data pour requete html/text

--- serveur_node_js/src/routes/data.js
+++ serveur_node_js/src/routes/data.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
+/* Requete HTTP GET sur /data */
 router.get('/', (req, res)=>{
-	const content_type = req.headers;
-	console.log(content_type)
-	const url = req.url
-	console.log(url);
-	res.render('data', { title: 'Hey', message: 'Hello there!'});
+	const accept = req.headers['accept'];
+
+	if(accept === "text/html")
+	{
+		res.setHeader('Content-Type', 'text/html');
+		res.sendFile(path.join(__dirname+'/../html/data.html'));
+	}else if (accept == "application/rdf+xml"){
+		console.log("RDF")
+	}
+	else {
+		//TODO
+		console.log("Aucun connu")
+	}
+	//res.render('data', { title: 'Hey', message: 'Hello there!'});
 });
 
 module.exports = router;
