5f0cc4d768136627c643795cbfa4b6b44b84aeb7
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-12-03 16:40:07 +0100

    WIP : Created files for RDF

--- serveur_node_js/src/routes/data.js
+++ serveur_node_js/src/routes/data.js
@@ -3,21 +3,18 @@ const router = express.Router();
 const path = require('path');
 
 /* Requete HTTP GET sur /data */
-router.get('/', (req, res)=>{
-	const accept = req.headers['accept'];
+router.get('/', (req, res) => {
+    const accept = req.headers['accept'];
 
-	if(accept === "text/html")
-	{
-		res.setHeader('Content-Type', 'text/html');
-		res.sendFile(path.join(__dirname+'/../html/data.html'));
-	}else if (accept == "application/rdf+xml"){
-		console.log("RDF")
-	}
-	else {
-		//TODO
-		console.log("Aucun connu")
-	}
-	//res.render('data', { title: 'Hey', message: 'Hello there!'});
+    if (accept === "text/html") {
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(path.join(__dirname + '/../html/data.html'));
+    } else if (accept === "application/rdf+xml") {
+        console.log("RDF")
+    } else {
+        return res.status(200).send("Type non reconnu").end();
+    }
+    //res.render('data', { title: 'Hey', message: 'Hello there!'});
 });
 
 module.exports = router;
